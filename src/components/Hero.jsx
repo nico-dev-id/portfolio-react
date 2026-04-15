c80b412b3cd7faa431e3e0449edaeb10c8ca2938
nico-dev-id <nico13la@gmail.com>
2026-04-15 15:53:04 +0700

    update: improve project descriptions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <div className="hero" id="hero">
         <h1>Hi, I'm Nico</h1>
         <h2>Frontend Developer</h2>
-        <p>I build modern and responsive web applications using React.</p>
+        <p>I build modern, responsive, and user-friendly web applications using React, focusing on clean UI and performance.</p>
 
         <div className="hero-buttons">
           <a href="#projects" className="btn-primary">View Project</a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,7 +33,9 @@ function Projects () {
             <div className="projects-container">
                 <ProjectCard 
                  title="Weather App" 
-                 desc="React weather app with real-time API and forecast."
+                 desc="A weather application built with React that fetches real-time data 
+                 from an external API. It allows users to search by city name, view current weather conditions, 
+                 and see a multi-day forecast with a responsive and dynamic UI."
                  live="https://weather-react-app-three-teal.vercel.app/"
                  github="https://github.com/nico-dev-id/weather-react-app"
                 image={weatherImg}
@@ -41,17 +43,21 @@ function Projects () {
 
                 <ProjectCard 
                  title="Portfolio Website" 
-                 desc="Personal portfolio built with React."
-                 live="#"
-                 github="#"
+                 desc="A personal portfolio website built with React to showcase projects and frontend skills. 
+                 Features include smooth scrolling navigation, interactive UI elements, and a responsive design 
+                 optimized for both desktop and mobile devices."
+                 live="https://portfolio-react-ten-virid.vercel.app/"
+                 github="https://github.com/nico-dev-id/portfolio-react"
                  image={portfolioImg}
                 />
 
                 <ProjectCard 
                  title="Task Manager App" 
-                 desc="A responsive task management application with drag-and-drop functionality, filtering system, and persistent data using LocalStorage."
-                 live="#"
-                 github="#"
+                 desc="A task management application built with JavaScript that allows users to create, organize, 
+                 and manage tasks efficiently. It includes drag-and-drop functionality, filtering options, 
+                 and persistent data storage using LocalStorage."
+                 live="https://nico-dev-id.github.io/task-manager-app/"
+                 github="https://github.com/nico-dev-id/task-manager-app"
                  image={todoImg}
                 />
             </div>
